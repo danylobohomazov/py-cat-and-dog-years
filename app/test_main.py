042d02b922bfd8e32e771f60e696bafd97ab61d2
Danylo <danil.bogomazov1356@gmail.com>
2026-02-12 13:43:47 +0200

    Change

--- app/test_main.py
+++ app/test_main.py
@@ -7,20 +7,24 @@ from app.errors import OutOfRangeError
 @pytest.mark.parametrize(
     'human_cat_age,human_dog_age,expected_result',
     [
-        pytest.param(13, 13, [0, 0], id="Ages < 15"),
-        pytest.param(22, 22, [1, 1], id="Ages >= 15 and Ages < 24"),
-        pytest.param(100, 100, [21, 17], id="Ages >= 24"),
+        pytest.param(0, 0, [0, 0], id="Ages 0"),
+        pytest.param(14, 14, [0, 0], id="0 < Ages < 15"),
+        pytest.param(15, 15, [1, 1], id="Ages == 15"),
+        pytest.param(23, 23, [1, 1], id="15 < Ages < 24"),
+        pytest.param(24, 24, [2, 2], id="Ages == 24"),
+        pytest.param(28, 28, [3, 2], id="Ages > 24"),
     ])
 def test_logic_for_different_ages(human_cat_age, human_dog_age, expected_result) -> None:
-    assert get_human_age(human_cat_age, human_cat_age) == expected_result
+    assert get_human_age(human_cat_age, human_dog_age) == expected_result
 
 
 @pytest.mark.parametrize(
     'human_cat_age,human_dog_age,expected_result',
     [
-        pytest.param(-13, 13, OutOfRangeError, id="Negative numbers"),
+        pytest.param(-13, -13, OutOfRangeError, id="Negative numbers"),
         pytest.param(2222, 2222, OutOfRangeError, id="Very Large number"),
         pytest.param("2222", 2222, TypeError, id="Wrong type"),
+        pytest.param(2222, "2222", TypeError, id="Wrong type"),
     ])
 def test_logic_for_incorrect_numbers(human_cat_age, human_dog_age, expected_result) -> None:
     with pytest.raises(expected_result):
